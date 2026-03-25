8a6d96c0569540438b749a88ca33474c47f2d1b9
Gust4av0 <gustavomarcolin2005@gmail.com>
2026-03-25 11:09:21 -0300

    chore: alterado o database

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,11 +3,11 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
-  process.env.DB_PASS as string,
+  process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
     port: Number(process.env.DB_PORT),
+    dialect: "mysql",
   }
 );
 
